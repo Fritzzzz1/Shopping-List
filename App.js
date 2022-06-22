40d958147afb10f45621727f290f99ce9d5e71b1
liran friedman <friedmanliran@gmail.com>
2022-06-22 23:25:21 +0300

    now really

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import MenuButton from './Components/MenuButton';
+import { example } from './data/exampleList';
+import Item from './Components/Item';
+import SearchBar from './Components/SearchBar';
 
 export default function App() {
+  console.log(example)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.page}>
+        <Text style={styles.textmain}> רשימת קניות </Text>
+        <ScrollView contentContainerStyle = {styles.frame}>
+          {example.map((item) => {
+            return (<Item key={item} product={item}/>)
+          })}
+        </ScrollView>
+        <SearchBar/>
+        <View style={styles.navbar}>
+          <MenuButton title='חדש'/>
+          <MenuButton title='רגיל'/>
+          <MenuButton title='הסטוריה'/>
+          <MenuButton title='עריכה'/>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +31,41 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#6fffe9',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  textmain: {
+    fontSize: 30,
+    alignItems: 'center',
+  },
+  textsecond: {
+    fontSize: 25,
+  },
+  page: {
+    marginTop: 40,
+    // backgroundColor: '#9dedad',
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  frame: {
+    backgroundColor: '#fff',
+    justifyContent: 'space-between',
+    marginTop: 20,
+    borderWidth: 1,
+    borderColor: 'black',
+    width: 310,
+    height: 420,
+    contentOffset: {x:10, y:10},
+  },
+  navbar: {
+    backgroundColor: '#FB836F',
+    borderWidth: 1,
+    borderColor: 'black',
+    height: 150,
+    width: 375,
+    flex: 1,
+    flexDirection: 'row',
+    
+  },
+
 });
---
+++ Styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet} from 'react-native';
+
+ const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'blue',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+     menu: {
+         flex: 1,
+         width:100,
+         height:100,
+         backgroundColor: 'blue',
+         alignItems: 'center',
+     }
+});
+
+
+export default styles;
